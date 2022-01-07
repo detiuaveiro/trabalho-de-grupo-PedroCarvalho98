1d97d74e1ea86366408e7f37c0d9cbc153b4fb8f
Pinh0 <t.pinho@ua.pt>
2022-01-07 22:24:50 +0000

    Primeira Entrega Trabalho de Grupo (07/01/2022)

--- myarmy.py
+++ myarmy.py
@@ -1,3 +1,5 @@
+#-----------IMPORTS-------------
+from os import sendfile
 import sys
 from typing import Tuple
 
@@ -9,9 +11,14 @@ from utils import _PRINT
 import json
 import numpy as np
 import math
+#-------------------------------
 
+#-------GLOBAL VARIABLES--------
+global flags
+#-------------------------------
+
+#------------DEBUG--------------
 DEBUG_FILE = "client_debug.txt"
-
 def debug(*args):
     _PRINT(*args, file=sys.stderr, flush=True)
     with open(DEBUG_FILE, 'a') as f:
@@ -19,8 +26,10 @@ def debug(*args):
         _PRINT(*args)
         sys.stdout = stdout
 
-print = debug # dont erase this line, otherwise you cannot use the print function for debug 
+print = debug # dont erase this line, otherwise you cannot use the print function for debug
+#-------------------------------
 
+#-----------ACTIONS-------------
 def upgradeBase():
     return f"{UPGRADE_BUILDING}"
 
@@ -32,9 +41,9 @@ def moveSoldiers(pos, to, amount):
 
 def playActions(actions):
     _PRINT(';'.join(map(str,actions)), flush=True)
+#-------------------------------
 
-
-
+#-------------------------------
 # ENVIRONMENT
 class Environment:
     def __init__(self, difficulty, base_cost, base_prod):
@@ -44,7 +53,13 @@ class Environment:
         self.base_cost = base_cost
         self.base_prod = base_prod
         self.board = [[None]*WIDTH for h in range(HEIGHT)]
+        self.turn = 1
+        self.retard = 0
+        self.spawn_soldiers = 2 
         playActions([])
+
+    def rounds_to_wait(self):
+        return self.upgrade_cost/(self.production*0.2)
 
     @property
     def upgrade_cost(self):
@@ -58,7 +73,7 @@ class Environment:
 
     def readEnvironment(self):
         state = input()
-        
+
         if state in ["END", "ERROR"]:
             return state
         level, resources, board = state.split()
@@ -76,9 +91,11 @@ class Environment:
         self.board = np.swapaxes(np.array(json.loads(board)),0,1)
         debug(self.board.shape)
 
-    #----------------------------------
+    #---------------------------------
 
-    def verifyproximity(self, my_position):
+    #--------FLAGS FUNCTIONS----------
+
+    def verifyproximity(self, my_position, flags):
         # --------FUNCTION-----------
         # > def verifyproximity()
         # --------DESCRIPTION--------
@@ -86,8 +103,9 @@ class Environment:
         # If an enemy is closer than 4 cells of distance, it returns a flag to indicate that there are enemies nearby.
         # --------INPUTS-------------
         # > my_position (from a certain entity)
+        # > flags (dictionary of the available flags)
         # --------OUTPUTS------------
-        # > flag proximity_enemies
+        # > flags
         # ---------------------------
 
         enemies = np.where(self.board[:,:,0]==ENEMY_SOLDIER_MELEE)
@@ -100,17 +118,15 @@ class Environment:
             #print("Enemy Position:", enemy_position)
             difference = (my_position[0] - enemy_position[0], my_position[1] - enemy_position[1])
             if abs(difference[0])<=4 and abs(difference[1])<=4:
-                proximity_enemies = 1
+                flags.update({"proximity_enemies":1})
+                print("Enemy Detected")
             else:
-                proximity_enemies = None
+                flags.update({"proximity_enemies":None})
 
-        return proximity_enemies
 
-    #----------------------------------
+    #--------ACTION FUNCTIONS----------
 
-    #--------ACTION FUNCTIONS---------- 
 
-   
     def ActionRange(self,entities,flags):
         # --------FUNCTION-----------
         # > def ActionRange()
@@ -121,27 +137,62 @@ class Environment:
         # > entities, flags
         # --------OUTPUTS------------
         # > moveaction
-        # ---------------------------  
+        # ---------------------------
+        attack_radius=[]   
+        for i in range (-3,4):
+            for n in range (-3,4):
+                if abs(n)+abs(i)==3:
+                    attack_radius.append((n,i))
+                    
+        enemies=[]
+        for attack in attack_radius:
+            attack_x,attack_y=attack
+            coord_x,coord_y=entities[0][0]+attack_x, entities[0][1]+attack_y
+            if not (0<=coord_x<WIDTH and 0<=coord_y<HEIGHT):
+                continue
+            target=self.board[coord_x,coord_y]
+            if target[0]==ENEMY_SOLDIER_MELEE:   
+                enemies.append(target[1])
+        print(enemies)
 
-        if entities[0][1]!=0: #Sobe sempre os Ranged
-            here=entities[0]
-            togo=(entities[0][0],entities[0][1]-1)
-            soldamount=entities[1][1]
-            moveaction = moveSoldiers(here,togo,soldamount)
-            return moveaction
-        #Mover Ranged para direita    
-        if entities[0][1]==0 and\
-        (self.board[entities[0][0]+1,entities[0][1],0]==ALLIED_SOLDIER_RANGED or self.board[entities[0][0]+1,entities[0][1],0]==None): #Adicionar Proximity Flag
-            here=entities[0]
-            togo=(entities[0][0]+1,entities[0][1])
-            soldamount=math.ceil(entities[1][1]/3)
-            if soldamount<=1:
-                pass
-            moveaction = moveSoldiers(here,togo,soldamount)
-            return moveaction
+        if entities[0]== (1,VCENTER) or entities[0]==(1,VCENTER-1) or entities[0]== (0,VCENTER-1) :
+            return None
+        else:
+            if entities[0][1]!=0: #Caso não esteja no topo da board
+                here=entities[0]
+                togo=(entities[0][0],entities[0][1]-1)
+                soldamount=entities[1][1]
+                sendamount=soldamount
+                if enemies!=[]:
+                    sendamount=min(sendamount,soldamount-max(enemies))
+                    if self.board[togo[0],togo[1],0]==ENEMY_SOLDIER_MELEE:          
+                        return None
+                if sendamount<=0:
+                    return None
+                moveaction = moveSoldiers(here,togo,sendamount)
+                return moveaction
+            #Mover Ranged para direita
+            if entities[0][1]==0 and self.turn>10:
+                here=entities[0]
+                togo=(entities[0][0]+1,entities[0][1])
+                soldamount=entities[1][1]
+                if (here[0]+3<WIDTH and self.board[here[0]+3,here[1],0]==ALLIED_SOLDIER_RANGED): #Caso seja da parte de trás do grupo
+                    sendamount=soldamount
+                else: #Seja da frente ou meio
+                    if soldamount<7:
+                        return None
+                    sendamount=math.ceil(soldamount/2)
+                if enemies!=[]:
+                    sendamount=min(sendamount,soldamount-max(enemies))
+                if self.board[togo[0],togo[1],0]==ENEMY_SOLDIER_MELEE:          
+                    return None
+                if sendamount<=0:
+                    return None
+                moveaction = moveSoldiers(here,togo,sendamount)
+                return moveaction
         return None
-    
-    #---------------------------------- 
+
+    #----------------------------------
 
     def ActionMelee(self,entities,flags):
         # --------FUNCTION-----------
@@ -152,14 +203,26 @@ class Environment:
         # > entities, flags
         # --------OUTPUTS------------
         # > moveaction
-        # --------------------------- 
-
+        # ---------------------------
         if entities[0][1]!=10: #Descer Sempre os Melee
             here=entities[0]
             togo=(entities[0][0],entities[0][1]+1)
             soldamount=entities[1][1]
             moveaction = moveSoldiers(here,togo,soldamount)
-            return moveaction
+            if self.turn < 1 or self.turn>8:
+                return moveaction
+        if entities[0][1]==10:
+            here=entities[0]
+            togo=(entities[0][0]+1,entities[0][1])
+            soldamount=entities[1][1]
+            sendamount=20
+            if soldamount<1:
+                return None
+            if soldamount<20:
+                sendamount=soldamount
+            moveaction = moveSoldiers(here,togo,sendamount)
+            if self.turn < 1 or self.turn>8:
+                return moveaction
         return None
 
     #----------------------------------
@@ -173,53 +236,105 @@ class Environment:
         # > None
         # --------OUTPUTS------------
         # > None
-        # --------------------------- 
+        #----------------------------
 
-        #----------------------------------
-        # TO-DO FLAGS
-        flags=dict()
-        # flag to verify if the cell to go is empty (first - empty, second - type)
-        available_cell = [None,None]
-        
-        # flag to verify if there are sufficient resources to buy base upgrade
-        resources_qntbase = 0
-        
-        # flag to verify if there are sufficient resources to buy x melees
-        resources_qntmelees = 0
-        
-        # flag to verify if there are sufficient resources to buy x ranges
-        resources_qntranges = 0
-        
-        # flag to verify the progress of the game (early game, mid game, late game)
-        game_progress = None
-
-        # flag to verify the proximity of enemies for ranges and other for melees (first - ranges, second - melees)    
-        proximity_enemies= None
-        
-        # flag for retard (20000 max)
-        retard = 0
-
-        #----------------------------------
+        #----------------------------
         # INITIALIZATION
+
+        #-----------FLAGS------------
+
+        flags = {
+            "available_cell" : [None,None], # flag to verify if the cell to go is empty (first - empty, second - type)
+            "bought_qntmelees" : 0, # flag to verify the quantity that was bought of melees
+            "bought_qntranges" : 0, # flag to verify the quantity that was bought of ranged
+            "game_progress" : None, # flag to verify the progress of the game (early game, mid game, late game)
+            "proximity_enemies" : None, # flag to verify the proximity of enemies for ranges and other for melees (first - ranges, second - melees)
+            "retard" : 0 # flag for retard (20000 max)
+        }
+
+        
+        #-----------------------------
+
+        #-------INFORMATIONS----------
 
         actions = []
         print("Current production per turn is:", self.production)
         print("Current building cost is:", self.upgrade_cost)
-        if self.resources >= self.upgrade_cost: # upgrade building
+        print(f"Need to survive {self.rounds_to_wait()} rounds to be profitable to upgrade the base!")
 
-            actions.append(upgradeBase())
-            self.resources -= self.upgrade_cost
+        #-----------------------------
+        
+        #-------Calculations----------
+        last_collum = self.board[WIDTH-1,:]
+        for a in last_collum:
+            if a[0] == ALLIED_SOLDIER_RANGED or a[0] == ALLIED_SOLDIER_MELEE:
+                self.retard += a[1]/5.0
+        self.spawn_soldiers = 2 + int((max(self.turn - self.retard, self.turn/3)**2)/65)         
 
-        # only buy ranged
-        default_cell_s_type = self.board[1,VCENTER,0]  # in numpy would be self.board[1,VCENTER,0]
-        if self.resources>=SOLDIER_MELEE_COST*(self.building_level+1) and default_cell_s_type in [EMPTY_CELL, ALLIED_SOLDIER_MELEE]:
-           # buyamount = self.resources//SOLDIER_RANGED_COST
-           print(str('\n')+str(self.building_level)+str("\n"))
-           buyamount=self.building_level+1
-           actions.append( recruitSoldiers(ALLIED_SOLDIER_MELEE, buyamount) )
-           self.resources -= buyamount*SOLDIER_MELEE_COST
+        print("Current retard: ", self.retard)
+        print("Current enemies spawning: ", self.spawn_soldiers)
+        retard_max = (10/3) * self.turn
 
-        #----------------------------------
+        #-----------------------------
+
+        #----------ECONOMY------------
+
+        #---------EARLY GAME----------
+        
+        #----------ECONOMY------------
+
+        #---------EARLY GAME----------
+        # Untill base level <= 8 upgrade base if possible
+        if self.building_level <= 6  or self.resources >= 3*self.upgrade_cost:
+            if self.resources >= self.upgrade_cost:     # flag
+                actions.append(upgradeBase())
+                self.resources -= self.upgrade_cost
+        #-----------------------------
+
+        #----------MID GAME-----------
+        # Only buy ranged from base level > 8 untill we have 100 ranged in front of the base
+        front_base_type = self.board[1,VCENTER,0]
+        front_base_quant = self.board[1,VCENTER,1]
+        if self.building_level > 6 and front_base_type in [EMPTY_CELL, ALLIED_SOLDIER_RANGED] and self.resources>=SOLDIER_RANGED_COST and front_base_quant <= 0:
+            ranges_amount = (self.resources//SOLDIER_RANGED_COST)
+            actions.append(recruitSoldiers(ALLIED_SOLDIER_RANGED, ranges_amount))
+            self.resources -= ranges_amount * SOLDIER_RANGED_COST
+            #melees_amount = (self.resources//SOLDIER_MELEE_COST)
+            #if melees_amount > 8:
+            #    melees_amount == 8
+            #actions.append(recruitSoldiers(ALLIED_SOLDIER_MELEE, melees_amount, (0, VCENTER+1)))
+            #self.resources -= melees_amount * SOLDIER_MELEE_COST            
+        #-----------------------------
+        
+        # When we have 100 ranges in front of base buy more ranges to the cell over the base
+        if self.building_level > 6 and front_base_type in [EMPTY_CELL, ALLIED_SOLDIER_RANGED] and self.resources>=SOLDIER_RANGED_COST and front_base_quant > 0:
+            ranges_amount = ((self.resources-SOLDIER_MELEE_COST*5)//SOLDIER_RANGED_COST)
+            actions.append(recruitSoldiers(ALLIED_SOLDIER_RANGED, ranges_amount, (1, VCENTER)))
+            self.resources -= ranges_amount * SOLDIER_RANGED_COST
+            pos = (1, VCENTER)
+            to = (1, VCENTER-1)
+            amount  = front_base_quant - 0
+            actions.append(moveSoldiers(pos, to, amount))
+
+        print(self.board[:,:,0]==ALLIED_SOLDIER_RANGED)
+
+        if self.board[1, VCENTER-1,1]>0:
+            pos = (1, VCENTER-1)
+            to = (1, VCENTER-2)
+            amount = self.board[1, VCENTER - 1, 1]
+            if amount>50:
+                actions.append(moveSoldiers(pos, to, 50))
+
+        if sum(self.board[self.board[:,:,0]==ALLIED_SOLDIER_RANGED,1]) > 150:
+            
+            melees_amount = 5
+            actions.append(recruitSoldiers(ALLIED_SOLDIER_MELEE, melees_amount, (0, VCENTER+1)))
+            self.resources -= melees_amount * SOLDIER_MELEE_COST   
+        
+        
+        #-----------------------------
+
+        #-----------------------------
         # MAP SEARCH
 
         print("Entity: ")
@@ -232,33 +347,33 @@ class Environment:
             entities[i].append((soldtype,quant))
         print("Entity results:", entities)
 
-        #-----------------------------------
+        #-----------------------------
         # ACTIONS
-
+        
         for i in range(len(entities)):
-            my_position = entities[i][0]
-            self.verifyproximity(my_position)
             if entities[i][1][0]==ALLIED_SOLDIER_RANGED:
-                moveaction=self.ActionRange(entities[i],flags)
-                if moveaction == None:
-                    continue
-                actions.append(moveaction)
-            elif entities[i][1][0]==ALLIED_SOLDIER_MELEE:
+              moveaction=self.ActionRange(entities[i],flags)
+              if moveaction == None:
+                  continue
+              actions.append(moveaction)
+            if entities[i][1][0]==ALLIED_SOLDIER_MELEE:
                 moveaction=self.ActionMelee(entities[i],flags)
                 if moveaction == None:
                     continue
                 actions.append(moveaction)
 
         playActions(actions)
+        self.turn += 1
+        #-----------------------------
+        
+#-------------------------------
 
-    #----------------------------------
-
-    
+#-------------MAIN--------------
 def main():
-    
+
     open(DEBUG_FILE, 'w').close()
     difficulty, base_cost, base_prod = map(int,input().split())
-   
+
     env = Environment(difficulty, base_cost, base_prod)
     while 1:
         signal = env.readEnvironment()
@@ -270,9 +385,10 @@ def main():
             sys.exit(1)
 
         env.play()
-        
+
 
 if __name__ == "__main__":
     main()
 
+#-------------------------------
 
